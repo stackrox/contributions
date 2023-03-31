cb85c7d4daad0f9002d1e8f31b923de100df9f1c
Chris Porter <chris@stackrox.com>
2023-03-31 11:05:54 -0400

    grace period for results from roxctl image scan

---
+++ util-scripts/roxctl-grace-period/grace.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from datetime import datetime, timedelta
+
+gracePeriod=30
+currentTime=datetime.now()
+
+image_json = json.load(sys.stdin)
+
+#print(image_json["scan"]["components"][0])
+
+for component in image_json["scan"]["components"]:
+    try:
+        for vuln in component["vulns"]:
+            try:
+                if vuln["fixedBy"]:
+                    pubTimestamp = datetime.strptime(vuln["publishedOn"][0:10], "%Y-%m-%d")
+                    if (currentTime - timedelta(days=gracePeriod)) > pubTimestamp:
+                        print ("out of grace: ", component["name"], vuln["cve"], vuln["severity"], vuln["fixedBy"], vuln["publishedOn"])
+                    else:
+                        print ("in grace: ", component["name"], vuln["cve"], vuln["severity"], vuln["fixedBy"], vuln["publishedOn"])
+                    #print (vuln["cve"])
+            except KeyError:
+                pass
+    except KeyError:
+        pass
